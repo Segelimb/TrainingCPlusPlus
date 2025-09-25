dc3b28ce565f313b276098fcff57d8c1b22d5280
Segelimb <zalilova84@gmail.com>
2025-09-25 11:23:00 +0400

    Minor changes

---
+++ project/MaxFour/MaxFour.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c, f,  x;
+    cin >> a >> b >> c >> f;
+    double m[4] = { a, b, c, f};
+    x = m[0];
+    for (int i = 1; i <= 3; i++)
+    {
+        if (x < m[i])
+        {
+            x = m[i];
+        }
+    }
+    cout << x;
+}
