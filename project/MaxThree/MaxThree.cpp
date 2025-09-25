91df4e397c4262f83900c2cd0a9195035cd8b5fb
Segelimb <zalilova84@gmail.com>
2025-09-25 11:17:03 +0400

    Minor changes

--- project/MaxThree/MaxThree.cpp
+++ project/MaxThree/MaxThree.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "");
 	double a, b, c, x;
 	cin >> a >> b >> c;
 	double m[3] = { a, b, c };
     x = m[0];
-    for (int i = 1; i <= 10; i++)
+    for (int i = 1; i <= 2; i++)
     {
         if (x < m[i])
         {
