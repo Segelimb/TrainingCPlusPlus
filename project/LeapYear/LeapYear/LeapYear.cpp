e02ef876f739975dac0a070a238e1cf9ba15c031
Segelimb <zalilova84@gmail.com>
2025-09-25 10:40:53 +0400

    Minor change

---
+++ project/LeapYear/LeapYear/LeapYear.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std; //позволяет не писать std и использовать endl вместо \n
+int main()
+{
+    setlocale(LC_ALL, "");
+    int year;
+    cin >> year;
+    if (year < 0)
+        cout << "Неверный номер года";
+    else
+    {
+        if ((year % 400) == 0)
+            cout << "Год " << year << " является високосным";
+        else if ((year % 4) == 0 && (year % 100) != 0)
+            cout << "Год " << year << " является високосным";
+        else 
+            cout << "Год " << year << " не является високосным";
+    }
+}
