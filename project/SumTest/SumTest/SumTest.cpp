85c3d9af5f713c1604023f03479d19637fdca31e
Segelimb <zalilova84@gmail.com>
2025-09-25 10:45:28 +0400

    minor changes

---
+++ project/SumTest/SumTest/SumTest.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    double a, b, c;
+    cout << "Введите 1-е число:";
+    cin >> a;
+    cout << "Введите 2-е число:";
+    cin >> b;
+    c = a + b;
+    std::cout << "Сумма = " << c;
+}
