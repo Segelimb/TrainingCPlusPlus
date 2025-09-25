8a0a455288f2e47a6e004832887ef74dc6be8c8b
Segelimb <zalilova84@gmail.com>
2025-09-25 10:57:09 +0400

    Minor change

---
+++ project/BodyMovement/BodyMovement/BodyMovement.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	double x_zero, v_zero, t, a, x;
+	cin >> x_zero >> v_zero >> t >> a;
+	x = x_zero + v_zero * t + ((a * t * t) / (2));
+	cout << "При x0 = " << x_zero << " v0 = " << v_zero << " t = " << t << " a = " << a << " тело находится в позиции " << x;
+}
