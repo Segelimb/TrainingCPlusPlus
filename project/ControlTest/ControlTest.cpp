27d4c898fdaee156b65c8b5d9f51fe1463333974
Segelimb <zalilova84@gmail.com>
2025-10-18 09:06:52 +0400

    add ControlTest

---
+++ project/ControlTest/ControlTest.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float x = -6.7;
+    float y = x / 2;
+    if (x >= -10)
+    {
+        x = x + 1;
+        y = x + 2;
+    }
+    else
+        x = x - 1;
+    cout << y;
+    cout << x << y;
+}
