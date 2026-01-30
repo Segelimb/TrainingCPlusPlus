3484b4fde9e53d4462c47087ac731cbe3f3c2a1b
Segelimb <zalilova84@gmail.com>
2026-01-30 09:02:14 +0400

    adds semester 2

---
+++ semester 2/trainingCPP2Semester/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int sumArray(int array[], int size)
+{
+	size--;
+	if (size == 0) return array[size];
+	else sumArray(array, size - 1);
+}
+
+int main()
+{
+	int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int sum = sumArray(array, 10);
+	cout << sum;
+}
